5c0f8f5569a131d762d5921aadd46e8dc8de9feb
TanPhuc-GH <truongtanphuc2002@gmail.com>
2023-05-14 15:41:16 +0700

    first commit

---
+++ src/main/java/com/hcmute/MobilePhoneShop/MobilePhoneShopApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.MobilePhoneShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePhoneShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePhoneShopApplication.class, args);
+	}
+
+}
